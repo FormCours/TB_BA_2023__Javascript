e20b94f8a7a32e8420443afe8492cc961f130682
Aurélien <aurelien.strimelle@bstorm.be>
2023-04-26 14:11:08 +0200

    Rappel

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+console.log('Hello les BA ♥');
+
+// Rappel 01
+const louis = document.createElement('p');
+louis.innerText = 'Hello Technobel';
+
+const commentaire = document.createComment('Ceci est un commentaire pour Tarik')
+
+const container = document.getElementById('container');
+container.appendChild(louis);
+container.appendChild(commentaire);
