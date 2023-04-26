5323266605d45555ed9dbeaec5c6b8c2bbf24384
Aurélien <aurelien.strimelle@bstorm.be>
2023-04-26 14:13:19 +0200

    Demo - Append / Prepend

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,22 @@ const commentaire = document.createComment('Ceci est un commentaire pour Tarik')
 const container = document.getElementById('container');
 container.appendChild(louis);
 container.appendChild(commentaire);
+
+
+/**************************************************************/
+const tarik = document.createElement('p');
+tarik.innerText = 'Tarik （⊙ｏ⊙）';
+
+const serge = document.createElement('p');
+serge.innerText = 'Serge 😎';
+
+const containerInsert = document.getElementById('container-insert');
+// Ajout en premier dans le "container"
+// containerInsert.prepend(tarik, serge)
+
+// Ajout en dernier dans le "container"
+containerInsert.append(tarik, serge)
+
+// Permet de supprimé un noeud
+const test = document.getElementById('test');
+test.remove()
